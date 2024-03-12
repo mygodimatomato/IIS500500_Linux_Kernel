51f12f87136d9aaa9771984775d2856dbefc5389
mygodimatomato <kullonono@gmail.com>
2024-03-12 16:53:52 +0800

    test version 3

--- HW/HW3/my_process.c
+++ HW/HW3/my_process.c
@@ -9,15 +9,14 @@ MODULE_LICENSE("Dual BSD/GPL");
 struct task_struct *task;  //Structure defined in sched.h for tasks/processes
 struct task_struct *task_child; // Structure needed to iterate through task children
 struct list_head *list; //Structure needed to iterate through the list in each task->children struct
-struct cred *cred; //Structure needed to access the credentials of each task 
 
 
 static int process_init(void)
 {
         printk(KERN_INFO "%s","LOADING MODULE\n");
         for_each_process( task ){  // for_each_process() MACRO for iterating through each task in the os located in linux/sched/signal.h
-                cred = get_task_cred(task); // get the credentials of the task
-                printk(KERN_INFO "PARENT PID: %d, UID: %d, PROCESS: %s\n",task->pid, cred->uid, task->comm); // log parent id/executable name/state
+                const struct cred *cred = get_task_cred(task); // get the credentials of the task
+                printk(KERN_INFO "PARENT PID: %d, UID: %d, PROCESS: %s\n",task->pid, cred->uid.val, task->comm); // log parent id/executable name/state
                 // list_for_each(list, &task->children){    //list_for_each MACRO to iterate through task->children
                 //         task_child = list_entry( list, struct task_struct, sibling );    //using list_entry to declare all vars in task_child struct
                 //         printk(KERN_INFO "\tCHILD OF %s[%d] PID: %d PROCESS: %s STATE: %ld\n",task->comm, task->pid, //log child of and child pid/nam
